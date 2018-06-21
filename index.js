814a65b9b402cd5c5da6e05a513825f9583c8810
soulos410 <solimr111@mail.ru>
2018-06-21 03:34:00 +0300

    landing/feature: add screenshots, slider

--- index.js
+++ index.js
@@ -7,3 +7,28 @@ $('a[href^="#"]').on('click', function(event) {
         }, 900);
     }
 });
+
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    let i;
+    let x = $('.screenshots__slider-image');
+    if (n > x.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = 'none'; 
+    }
+    x[slideIndex-1].style.display = 'block'; 
+}
+
+$('.screenshots__previous-image').click(() =>{
+    plusDivs(-1);
+});
+$('.screenshots__next-image').click(() =>{
+    plusDivs(+1);
+});
